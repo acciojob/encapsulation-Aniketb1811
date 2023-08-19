73441ef60a0450f30307133e2a64ebcce2a86a10
Aniket Birambole <aniketb1811@gmail.com>
2023-08-19 22:26:41 +0530

    Commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String[] args){
+      RWOnly rwOnly = new RWOnly();
+//      rwOnly.name = "Aniket";
+      //name variable is private
+
+      rwOnly.setName("Aniket");
+      System.out.println(rwOnly.getName());
+    }
 }
